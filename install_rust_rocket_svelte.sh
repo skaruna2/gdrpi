25e9b871c3be934d1b280b67b2aac1aacbebde38
hkreie <91433473+hkreie@users.noreply.github.com>
2021-11-13 16:44:18 -0700

    Create install_rust_rocket_svelte.sh

---
+++ install_rust_rocket_svelte.sh
@@ -0,0 +1,18 @@
+# Install rust-rocket-svelte app framework from https://github.com/joemooney/rust-rocket-svelte on a new Raspberry Pi
+echo "Install curl"
+sudo apt install curl
+echo "Install git"
+sudo apt install git
+echo "Install npm"
+sudo apt install npm
+echo "Install rust and cargo (Rust's package manager)"
+curl https://sh.rustup.rs -sSf | sh
+echo "clone, or better yet your github cli tool"
+git clone https://github.com/joemooney/rust-rocket-svelte
+echo "Add rust to env"
+source $HOME/.cargo/env
+echo "Set rust auto-updates"
+#rustup override set nightly
+rustup default nightly-2021-10-05
+cd rust-rocket-svelte
+npm install
